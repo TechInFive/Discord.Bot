3cec563c5cee7eb0ee68fd39d25f88e2a93d4a0b
TechInFive <quick.tech.in.five@gmail.com>
2024-04-15 17:45:29 -0400

    Final Demo

--- Bot.py
+++ Bot.py
@@ -3,6 +3,7 @@ import random
 import discord
 
 from ChatService import ChatService
+from OpenAIService import OpenAIService
 
 # Jokes
 python_jokes = [
@@ -24,10 +25,16 @@ intents.message_content = True  # Enable message content
 # Initialize the client with the defined intents
 client = discord.Client(intents=intents)
 
-# Setting a custom prefix
-custom_prefix = "!"
+# Attempt to load the Opus library
+if not discord.opus.is_loaded():
+    # The name of the Opus library may vary depending on your system
+    # Windows: opus.dll
+    # macOS: libopus.dylib
+    # Linux: libopus.so
+    discord.opus.load_opus(os.getenv('LIBOPUS_PATH'))
 
 chat_service = ChatService()
+open_ai_service = OpenAIService()
 
 @client.event
 async def on_ready():
@@ -46,19 +53,6 @@ async def on_message(message):
     # Ignore unsupported message types
     if message.type not in [discord.MessageType.default, discord.MessageType.reply]:
         return
-
-    # text - Test message @ 845081847410065418
-    # private - Test DM @ 1219276778581790770
-    # public_thread - Test Thread Replay @ 1222276084905803908
-    # public_thread Test Reply of Thread Message @ 1222276084905803908
-    # text - Test - Reply Channel Message @ 845081847410065418
-    # text - Test - Reply Channel Message @ 845081847410065418
-    # text - Create new thread @ 845081847410065418
-    # public_thread - First message of new thread @ 1222285886998446201
-
-    # MessageType.default
-    # MessageType.reply
-    # MessageType.thread_created
     
     content = message.content
     channel = message.channel
@@ -75,12 +69,17 @@ async def on_message(message):
         await channel.send(model_info)
     elif content == "!help":
         help_text = (
-            "**Bot Commands:**\n"
-            "- `!openai <query>`: Get responses from OpenAI.\n"
-            "- `!mistral <query>`: Get responses from Mistral AI.\n"
-            "- `!joke`: Display a Python Joke.\n"
-            "- `!modelinfo`: Get information about the AI models.\n"
-            "- `!help`: Display this help message."
+            "**Bot Commands Overview:**\n"
+            "- `!openai <query>`: Query OpenAI's models for detailed responses to complex questions.\n"
+            "- `!mistral <query>`: Access Mistral AI for quick, concise answers to your inquiries.\n"
+            "- `!dall-e <query>`: Use DALL-E to create images based on your textual descriptions.\n"
+            "- `!join`: Commands the bot to join the voice channel you are currently in.\n"
+            "- `!leave`: Instructs the bot to leave the current voice channel, ending any active participation.\n"
+            "- `!joke`: Enjoy a light-hearted Python joke to brighten your day.\n"
+            "- `!modelinfo`: Learn about the AI models powering the bot, including their features and uses.\n"
+            "- `!permissions`: Displays the bot’s permissions for transparency and trust.\n"
+            "- `!help`: Provides this message, listing all available commands and their functions.\n"
+            "Use these commands to enhance your interaction with the bot and explore its functionalities."
         )
         await channel.send(help_text)
     elif content == "!permissions":
@@ -97,9 +96,41 @@ async def on_message(message):
     elif content.startswith("!dall-e"):
         response_text = chat_service.draw_image(content)
         await channel.send(response_text)
+
+    elif content == "!join":
+        # Check if the bot is already in a voice channel in this guild
+        if message.guild.voice_client is not None:
+            await message.channel.send("I'm already in a voice channel.")
+            return
+
+        # Check if the user is in a voice channel
+        if message.author.voice and message.author.voice.channel:
+            voice_channel = message.author.voice.channel
+            await voice_channel.connect()
+            await message.channel.send(f"Joined {voice_channel.name}!")
+        else:
+            await message.channel.send("You need to be in a voice channel for me to join.")
+
+    elif content == "!leave":
+        # Check if the bot is in a voice channel in this guild
+        if message.guild.voice_client is not None:
+            await message.guild.voice_client.disconnect()
+            await message.channel.send("I've left the voice channel.")
+        else:
+            await message.channel.send("I'm not in any voice channel right now.")
+
     else:
         response_text = chat_service.handle_message(channel.id, content)
         if response_text != None:
             await channel.send(response_text)
+
+            # Check if the user is in a voice channel
+            if message.author.voice and message.author.voice.channel:
+                # Check if the bot is in a voice channel in this guild
+                if message.guild.voice_client is not None:
+                    open_ai_service.create_speech_file("echo", response_text, "ai_response.mp3")
+
+                    source = discord.FFmpegPCMAudio("ai_response.mp3")
+                    message.guild.voice_client.play(source, after=lambda e: print('Finished playing', e))
 
 client.run(BOT_TOKEN)
